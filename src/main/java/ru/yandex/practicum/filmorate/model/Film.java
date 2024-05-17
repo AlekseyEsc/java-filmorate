692ba2c8de7e43bf0b5afa6198eda857425476ff
AlekseyEsc <aleksey.andropovw@gmail.com>
2024-05-18 00:33:12 +0300

    add dto

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,16 +1,30 @@
 package ru.yandex.practicum.filmorate.model;
 
-
+import jakarta.validation.constraints.*;
+import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import ru.yandex.practicum.filmorate.validator.FilmReleaseDate;
 
-import java.time.Duration;
 import java.time.LocalDate;
 
 @Data
+@Builder
+@EqualsAndHashCode(of = {"id"})
 public class Film {
     private Long id;
+
+    @NotBlank
     private String name;
+
+    @NotNull
+    @Size(min = 3, max = 200)
     private String description;
+
+    @FilmReleaseDate
     private LocalDate releaseDate;
-    private Duration duration;
+
+    @NotNull
+    @Positive
+    private Integer duration;
 }
