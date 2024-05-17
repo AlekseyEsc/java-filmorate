8ba62b0e1f0a3d5cb9fa4ca6dda32014a6902275
AlekseyEsc <aleksey.andropovw@gmail.com>
2024-05-18 00:33:26 +0300

    add controllers

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,57 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.web.bind.annotation.*;
+
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+    private final Map<Long, Film> films = new HashMap<>();
+
+    @GetMapping
+    public Collection<Film> findFilms() {
+        return new ArrayList<>(films.values());
+    }
+
+    @PostMapping
+    public Film create(@Valid @RequestBody Film film) {
+        Long id = getNextId();
+        film.setId(id);
+        films.put(film.getId(), film);
+        log.info("Add new film " + film);
+        return film;
+    }
+
+    @PutMapping
+    public Film update(@Valid @RequestBody Film film) {
+        Long filmId = film.getId();
+        if (!films.containsKey(filmId)) {
+            throw new ValidationException("Film not found by id");
+        }
+        films.put(filmId, film);
+        log.info("Update film " + film);
+        return film;
+    }
+
+    private long getNextId() {
+        long currentMaxId = films.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+
+
 }
